c218dab335a8d712e275def0eb2dfcfea4b7d7a5
草苅　快 <kusakai@kusakai-no-MacBook-Air.local>
2015-08-08 23:23:08 +0900

    edit map regist.

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,8 +1,10 @@
 class Map
   include Cequel::Record
-
   key :id, :text
+  column :username, :text, :index => true
   column :mapname, :text, :index => true
+  column :lat, :text
+  column :lgn, :text
   column :body, :text
   column :clean, :int
   column :famous, :int
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get  "welcome/start" => "welcome#start"
-  get  "welcome/add"   => "welcome#add"
-  get  "welcome/map"   => "welcome#map"
-  get  "welcome/view"  => "welcome#view"
+  root  'welcome#index'
+  get   "welcome/start" => "welcome#start"
+  get   "welcome/add"   => "welcome#add"
+  get   "welcome/map"   => "welcome#map"
+  get   "welcome/view"  => "welcome#view"
+  post  "map/create"    => "map#create"
+  get   "map/"          => "map#index"
 end
