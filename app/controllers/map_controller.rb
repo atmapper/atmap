acc417ce713e510353d43d0c7e0c5670d96ea88d
草苅　快 <kusakai@kusakai-no-MacBook-Air.local>
2015-08-08 23:33:46 +0900

    edit map regist

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,24 @@
+class MapController < ApplicationController
+	def index
+	end
+	def create
+		idstr = Time.now.strftime("%Y%m%d%H%M%S") + (0..9).to_a.sample(5).join
+		t = Map.new(id: idstr);
+  		t.mapname = params[:mapname];
+  		t.lat = params[:lat];
+  		t.lgn = params[:lgn];
+  		t.body = '';
+  		t.clean = '1';
+  		t.famous = '2';
+  		t.infoseek = '3';
+    	if t.save
+    	  render :template => 'map/index' , :layout => false , :status => 200
+    	else
+    	  render :template => 'map/index' , :layout => false , :status => 400
+    	end
+	end
+	def show
+	end
+	def list
+	end
+end
